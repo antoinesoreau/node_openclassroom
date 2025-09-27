4d4e540d17ab5cebf6d3d3613c920577ddae62d3
Agide <aniotne@gmail.com>
2025-09-27 16:35:22 +0200

    Créez une application Express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-// const http = require('http'); // Import du module Node HTTP pour permetttre les requettes HTTP
+const http = require('http');
+const app = require('./app');
 
-// const server = http.createServer((req, res) => { // Creation d'un serveur qui reçoit des requetes (req) et envoie une réponse (res)
-//     res.end('réponse du server'); 
-// });
+app.set('port', process.env.PORT || 3000); // Définit le port d'écoute de l'application
+const server = http.createServer(app);
 
-// server.listen(process.env.PORT || 3000); // Le serveur écoute sur le port 3000 ou un port défini dans les variables d'environnement
-console.log('test');
+server.listen(process.env.PORT || 3000); // Le serveur écoute sur le port 3000 ou un port défini dans les variables d'environnement
